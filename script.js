ffb144b2af04edf2b835ea4a1b9a3862001a0745
Frankie <franklineokubasu51@gmail.com>
2024-05-01 17:39:28 +0300

    pushing i to the rows array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const character = "# ";
 const count = 8;
 const rows = [];
-
-for("iterator"; "condition"; "iteration"){}
-
+for(let i = 0; i < count; i = i + 1){
+    rows.push(i);
+}
+// the loop runs 8 times until the condition is reached
