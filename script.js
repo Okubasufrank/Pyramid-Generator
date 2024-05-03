8d74c2235c35683b76bc6967df2ace1e6cb9afdb
Frankie <franklineokubasu51@gmail.com>
2024-05-03 22:28:08 +0300

    using second addition operator to concatinate a new line

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const character = "# ";
 const count = 8;
 const rows = [];
+
+// the loop runs 8 times until the condition is re
 for(let i = 0; i < count; i = i + 1){
     rows.push(i);
 }
-// the loop runs 8 times until the condition is re
  let result = "";
+ console.log(result);
+
+ /* use of (for--of) loop
+    NB:it itterates over an item in an iterable object &
+    temporarily assigns it to a variable
+    -This {for...of} loop bellow will iterate through rows array,
+    assigning each value to a row variable
+    */
+for (const row of rows){
+    result = result + "\n" + row;
+ }
