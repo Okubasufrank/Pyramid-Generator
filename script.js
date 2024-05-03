12bededa0c115a53a3355272d0fdcb9e8b51e942
Frankie <franklineokubasu51@gmail.com>
2024-05-03 22:52:19 +0300

    use of .repeat() method

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const rows = [];
 
 // the loop runs 8 times until the condition is re
 for(let i = 0; i < count; i = i + 1){
-    rows.push(character);
+    rows.push(character); // use of .repeat() method
+    character.repeat(i);
+    /*
+     The above method accepts a number as an argument, specifying the number
+      of times to repeat the target string. For example, using .repeat()
+       to generate the string "Code! Code! Code!"
+    */
+    
 }
  let result = "";
  console.log(result);
@@ -13,8 +20,8 @@ for(let i = 0; i < count; i = i + 1){
     NB:it itterates over an item in an iterable object &
     temporarily assigns it to a variable
     -This {for...of} loop bellow will iterate through rows array,
-    assigning each value to a row variable
-    */
+    assigning each value to a row variable */
+
 for (const row of rows){
     result = result + "\n" + row;
  }
