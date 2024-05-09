53ea46b06beadc04e8199d0fae3b858bf26fa634
Frankie <franklineokubasu51@gmail.com>
2024-05-09 17:23:48 +0300

    passing a full expression as an argument

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount){
-    return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// this  loop runs 8 times until the condition is reached
+// this  loop runs 8 times until the condition is   reached
 for(let i = 0; i < count; i = i + 1){
     rows.push(padRow(i+1, count));
     //rows.push(character); 
