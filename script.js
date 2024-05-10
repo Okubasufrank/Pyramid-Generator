a9439362e70e60ee89c2686c2751d98f1a1ac069
Frankie <franklineokubasu51@gmail.com>
2024-05-10 11:37:54 +0300

    checking if inverted is true

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const character = "# ";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount){
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
@@ -9,6 +10,7 @@ function padRow(rowNumber, rowCount){
 // this  loop runs 8 times until the condition is   reached
 
 for(let i = 1; i <= count; i++ ){
+    if(inverted){}
     rows.unshift(padRow(i , count)); 
 }
 
