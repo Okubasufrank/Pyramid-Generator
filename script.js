84aa0666e6f9ee55437bebe41e23c2f1be174c76
Frankie <franklineokubasu51@gmail.com>
2024-05-10 09:29:48 +0300

    using the strict inequality operator to check if done is not equal to count

--- script.js
+++ script.js
@@ -16,10 +16,11 @@ for(let i = 1; i <= count; i++ ){
 
 let continueLoop = false;
 let done = 0;
-while(continueLoop){
+while(done !== count){
     done++;
+    rows.push(padRow(done, count));
   if(done === count){
-    
+    continueLoop = false;
   }
 
 }
